2d2fd74b75eede634986640a47e4641b9c75c6a1
constancemachiri <machiriconstance@gmail.com>
2024-10-09 21:55:11 +0200

    great User experience

--- index.js
+++ index.js
@@ -21,9 +21,11 @@ function generatePoem(event){
     let apiKey = "5451tfeo2c8a3b1e71a0f670f0558529";
     let prompt =  document.querySelector("#input").value;
     
-    let context = `you are a romatic poet and love to generate brief poem of not more than 15 lines about ${"input"}.value`
+    let context = `you are a romatic poet and love to generate brief poem of 10 lines about ${"input"}.value`
     let ApiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-    
+    let poems= document.getElementById("poemspace");
+    poems.classList.remove("hidden");
+    poems.innerHTML=`<div class="blink">⌛Generating poem about ${prompt}</div>`;
     axios.get(ApiUrl).then(results);
 }
 
